39b65afd2b0b3dfa02d9fefc338dc124a3ee16f2
Matt Wong <49925170+WongMatthew@users.noreply.github.com>
2023-04-01 16:24:15 -0700

    add img changes

--- zenchat/src/App.js
+++ zenchat/src/App.js
@@ -1,12 +1,12 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import React from 'react';
-import BurgerMenu from './components/HamburgerMenu.js';
 import './App.css';
 
 function App() {
   return (
     <div>
-      <BurgerMenu />
+      <img></img>
+      <img></img>
       <h1>Welcome to ZenChat</h1>
       <p>Your mental health chatbot and therapist.</p>
       {/* Rest of your app content goes here */}
