0faf3b4e4514f5ea08f93eca813ff2e7062aeabc
Matt Wong <49925170+WongMatthew@users.noreply.github.com>
2023-04-01 17:14:55 -0700

    remove hamburger menu and simplify

--- zenchat/src/App.js
+++ zenchat/src/App.js
@@ -5,11 +5,7 @@ import './App.css';
 function App() {
   return (
     <div>
-      <img></img>
-      <img></img>
-      <h1>Welcome to ZenChat</h1>
-      <p>Your mental health chatbot and therapist.</p>
-      {/* Rest of your app content goes here */}
+      hi
     </div>
   );
 }
